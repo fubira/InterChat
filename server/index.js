5676b9aab85bd97bc37deb576687cee43d5a1dfe
fubira <fubira@gmail.com>
2019-10-11 04:23:53 +0900

    web interfaces

--- server/index.js
+++ server/index.js
@@ -1,40 +1,60 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379'
-let Redis = require('ioredis');
-let redis = new Redis(REDIS_URL);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const REDIS_URL = process.env.REDIS_URL || 'redis://h:p7e3f731fa2fa7e59738c11534102195c276456cf3672539ff781042d66a8f91b@ec2-3-92-111-148.compute-1.amazonaws.com:22409'
+const Redis = require('ioredis');
+const redis = new Redis(REDIS_URL);
 
 app.set('port', (process.env.PORT || 5000))
 
-app.listen(app.get('port'), () => {
-  console.log("Node app is running at localhost:" + app.get('port'));
+const server = app.listen(app.get('port'), 'localhost', function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
 });
 
-let KEY = "logs";
+const KEY = "messages";
 
+app.get('/', (req, res) => {
+  res.json({ result: 'ok', time });
+});
 
 // Post message
-app.post('post', (req, res) => {
-  let body = req.body;
-  let now = DateTime.now();
-  /// sync.zadd(key, time, jsonString);
-  console.log(JSON.stringify(body));
-  // redis.zadd(KEY, now, body)
-  res.json({ result: 'ok' });
+app.post('/post', (req, res) => {
+  const time = Date.now();
+  const body = req.body;
+
+  redis.zadd(KEY, time, JSON.stringify(body)).then((result) => {
+    console.log(result);
+    res.json({ result: 'ok', time });
+  });
 });
 
-// Receive messages
-app.get('receive', (req, res) => {
-  let body = req.body;
-  console.log(JSON.stringify(body));
-  // List<ScoredValue<String>> scoredValue = sync.zrangebyscoreWithScores(key, Range.create(this.lastTime, Double.POSITIVE_INFINITY));
-  res.json({ result: 'ok' });
+// Get messages
+app.get('/message', (req, res) => {
+  const query = req.query;
+
+  redis.zrangebyscore(KEY, query.from, "+inf", "WITHSCORES").then((result) => {
+    let messages = [];
+    let lastTime = query.from;
+    while (result.length > 0) {
+      let value = result.shift();
+      lastTime = result.shift();
+      messages.push(JSON.parse(value));
+    }
+    res.json({ result: 'ok', time: lastTime, messages });
+  });
 });
 
 // Expire old messages
-app.get('expire', (req, res) => {
-  // private long expireMillis = 1000 * 60 * 10;
-  // sync.zremrangebyscore(key, Range.create(0, time - expireMillis));
-  res.json({ result: 'ok' });
+app.get('/expire', (req, res) => {
+  const time = Date.now();
+  const expireMillis = 1000 * 60 * 10;
+  redis.zremrangebyscore(KEY, 0, time - expireMillis).then((err, result) => {
+    res.json({ result: 'ok', time });
+  });
 });
