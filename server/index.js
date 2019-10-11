d1712738f24ce82bbaa379d397b37ec70316c882
fubira <fubira@gmail.com>
2019-10-12 07:46:27 +0900

    update port

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379'
 const Redis = require('ioredis');
 const redis = new Redis(REDIS_URL);
 
-app.set('port', (process.env.PORT || 5125))
+app.set('port', (process.env.PORT || 5000))
 
 const server = app.listen(app.get('port'), function () {
   const host = server.address().address;
